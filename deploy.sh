16467caab51bad3356c105d4344146668de43bb6
Class30-G16 <kayosho73@gmail.om>
2022-11-24 17:12:52 +0000

    Conflict3 resolved

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #This is a deployment script
 Deploy python apps
-deploy in the dev env
+deploy  the dev env
 deployment5 master
 deployment4
 deploy to staging
-echo good job.
 tested and accepted 
 development5
-development6
+development
